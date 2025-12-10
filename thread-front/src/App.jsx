d6a6e4103678f009b0ccc9b013dfce16868d97a8
Brouev <c.charlot333@gmail.com>
2025-12-10 12:03:52 +0100

    fix app.tsx

--- thread-front/src/App.jsx
+++ thread-front/src/App.jsx
@@ -1,31 +1,23 @@
-import "./App.css";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-
-import Feed from "./components/Feed.jsx";
-import Login from "./components/Login.jsx";
-import Register from "./components/Register.jsx";
-import Profil from "./components/Profil.jsx";
-import AddPost from "./components/AddPost.jsx";
-import PostDetailCard from "./components/PostDetailCard.jsx";
+import Feed from "./components/Feed/Feed.jsx";
+import Login from "./components/Login/Login.jsx";
+import Register from "./components/Register/Register.jsx";
+import PostDetailCard from "./components/PostDetailCard/PostDetailCard.jsx";
+import Profile from "./components/Profile/Profile.jsx";
+import AddPost from "./components/AddPost/AddPost.jsx";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
   return (
     <Routes>
-      {/* feed */}
-      <Route path="/" element={<Feed />} />
-
-      {/* auth */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-
-      {/* profil user connecté */}
-      <Route path="/profile" element={<Profil />} />
-
-      {/* add post */}
-      <Route path="/add-post" element={<AddPost />} />
-
-      {/* detail post */}
-      <Route path="/posts/:postId" element={<PostDetailCard />} />
+      <Route path="/" element={<Feed currentUser={currentUser} />} />
+      <Route path="/login" element={<Login setCurrentUser={setCurrentUser} />} />
+      <Route path="/register" element={<Register setCurrentUser={setCurrentUser} />} />
+      <Route path="/posts/:postId" element={<PostDetailCard currentUser={currentUser} />} />
+      <Route path="/profile" element={<Profile currentUser={currentUser} />} />
+      <Route path="/add-post" element={<AddPost currentUser={currentUser} />} />
     </Routes>
   );
 }
